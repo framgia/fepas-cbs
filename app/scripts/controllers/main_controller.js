c36b94fb6a5f2efc171c97deb3abc92440b8973a
Phương 'J' Lê H <phuonglh0420@gmail.com>
2016-04-21 10:46:18 +0000

    First skeleton for CBS

---
+++ app/scripts/controllers/main_controller.js
@@ -0,0 +1,16 @@
+/* global angular */
+
+(function(){
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name app.controllers:MainCtrl
+   * @description
+   * # MainCtrl
+   * Controller of the app
+   */
+  angular.module('app.controllers').controller('MainController', MainController);
+
+  function MainController() {}
+})();
